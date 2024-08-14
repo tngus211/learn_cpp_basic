957e455229e053e7418d2a14f728df70bdb07677
tngus <tngus00211@gmail.com>
2024-08-14 12:54:28 +0900

    fifth commit

--- ch05/z512.cpp
+++ ch05/z512.cpp
@@ -1,2 +1,18 @@
 //프로그래밍 문제 5-12
 //2000년부터 2099년까지의 범위에 있는 윤년을 출력하는 프로그램
+#include <iostream>
+using namespace std;
+int main()
+{
+    bool leapyear;
+    for (int year = 2000; year <= 2099; year++)
+    {
+        leapyear = (year % 400) || (year % 4) && !(year % 100);
+        if (leapyear == 0)
+        {
+            cout << year << "  ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
